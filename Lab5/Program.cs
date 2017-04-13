b79d5d1ec8492750c6832eade7c1f9e4b50f94b5
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-13 10:08:26 -0400

    Asks user to continue

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -10,8 +10,37 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            GetDiceSides("first");
-            GetDiceSides("second");
+            bool run = true;
+
+            Console.WriteLine("Welcome to the Grand Circus Casino! Would you like to roll the dice? (y/n)");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "n")
+            {
+                Console.WriteLine("Have a nice day!");
+                return;
+            }
+
+            do
+            {
+                GetDiceSides("first");
+                GetDiceSides("second");
+
+                //Roll Dice
+
+                Console.WriteLine("Do you want to continue rolling? (y/n)");
+
+                string input2 = Console.ReadLine();
+
+                if (input2.ToLower() == "n")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    run = false;
+                }
+
+            } while (run);
+            
             
         }
 
